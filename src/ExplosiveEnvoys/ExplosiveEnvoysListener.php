3a6457babe6b419d5e42a59c765cd9d1c8d2b13e
andreskomet <giantquartz@gmail.com>
2018-02-18 13:39:15 +0100

    Add GNU v3 General Public License

--- src/ExplosiveEnvoys/ExplosiveEnvoysListener.php
+++ src/ExplosiveEnvoys/ExplosiveEnvoysListener.php
@@ -1,9 +1,11 @@
 <?php
-/**
- * Copyright (C) A. Andrés R. Arias - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- */
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * @author GiantQuartz
+*/
 
 namespace ExplosiveEnvoys;
 
